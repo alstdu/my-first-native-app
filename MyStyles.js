36ee00a1a38c8a622f959f6bb819c91899b65a0a
alstdu <lexi.dugo@gmail.com>
2024-06-24 16:58:43 -0700

    Create background image and style

--- MyStyles.js
+++ MyStyles.js
@@ -1,17 +1,33 @@
 
 import { StyleSheet } from 'react-native';
-
+// GLOBAL STYLESHEET
 const MyStyles = StyleSheet.create({
-  button: {
+  ButtonContainer: {
     padding: 10,
-    backgroundColor: '#2196F3',
-    borderRadius: 100,
+    backgroundColor: '#366AF5',
+    borderRadius: 50,
+    width: 130,
+    marginTop: 10,
   },
-  buttonText: {
-    color: '#366AF5',
+  ButtonText: {
+    color: '#fff',
     textAlign: 'center',
     fontWeight: 'bold',
   },
+  Heading: {
+    fontWeight: 'bold',
+    fontSize: 36 ,
+    marginBottom: 10,
+    width: 150,
+  },
+  HomeBgImage: {
+    width: '100%',
+    height: '100%',
+    borderTopEndRadius: 190,
+    borderTopStartRadius: 190,
+    overflow: 'hidden',
+    marginTop: 70
+  }
 });
 
 export default MyStyles;
--- components/GetStartedScreen.js
+++ components/GetStartedScreen.js
@@ -1,15 +1,32 @@
 
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
+import MyStyles from "../MyStyles";
 
 export default function GetStartedScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Get Started"
-        onPress={() => navigation.navigate("SignUp")}
-      />
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, backgroundColor: '#fff' }}>
+            <ImageBackground
+                style={MyStyles.HomeBgImage}
+                source={require('../assets/splashyoga.jpg')}>
+            </ImageBackground>
+            <View style={{margin: 20, position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+                <Text
+                    style={MyStyles.Heading}
+                >
+                    Discover Yoga with Friends
+                </Text>
+                <Text>Discover a new way to stay connected and healthy with our revolutionary yoga app!
+                    Join live sessions, share your progress, and motivate each other in real-time.
+                    Download now and elevate your yoga practice together</Text>
+                <TouchableOpacity
+                    style={MyStyles.ButtonContainer}
+                    onPress={() => navigation.navigate("SignUp")}>
+                    <Text
+                        style={MyStyles.ButtonText}
+                    >Get Started</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
