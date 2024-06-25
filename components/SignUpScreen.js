4cc541f1331a00f091f8983ddaef65cc8910e3bc
alstdu <lexi.dugo@gmail.com>
2024-06-24 21:11:51 -0700

    Add placeholder text to input fields

--- components/SignUpScreen.js
+++ components/SignUpScreen.js
@@ -5,11 +5,11 @@ export default function SignUpScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Name</Text>
-      <TextInput style={MyStyles.input} />
+      <TextInput style={MyStyles.input} placeholder="Name" />
       <Text>Email</Text>
-      <TextInput style={MyStyles.input} />
+      <TextInput style={MyStyles.input} placeholder="Email" />
       <Text>Password</Text>
-      <TextInput style={MyStyles.input} />
+      <TextInput style={MyStyles.input} placeholder="Password" />
       <TouchableOpacity
         style={MyStyles.ButtonContainer}
         onPress={() => navigation.navigate("Invite")}>
