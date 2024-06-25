bf996f5b870631b1d64b0567c3a34e2564eac32c
alstdu <lexi.dugo@gmail.com>
2024-06-24 17:19:35 -0700

    Create button for sign up

--- components/SignUpScreen.js
+++ components/SignUpScreen.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
+import MyStyles from "../MyStyles";
 export default function SignUpScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Sign Up</Text>
-      <Button
-        title="Continue"
-        onPress={() => navigation.navigate("Invite")}
-      />
+      <TouchableOpacity
+        style={MyStyles.ButtonContainer}
+        onPress={() => navigation.navigate("Invite")}>
+          <Text>Continue</Text>
+      </TouchableOpacity>
     </View>
   );
 }
