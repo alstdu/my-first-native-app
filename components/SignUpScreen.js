609e57b3391225b78484ab3cb58badb2d8245fc7
alstdu <lexi.dugo@gmail.com>
2024-06-25 09:37:01 -0700

    Create heading styles

--- components/SignUpScreen.js
+++ components/SignUpScreen.js
@@ -3,9 +3,13 @@ import { TouchableOpacity, View, Text, TextInput } from "react-native";
 import MyStyles from "../MyStyles";
 export default function SignUpScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "flex-end", backgroundColor: "white" }}>
       <View>
-
+        <Text
+          style={MyStyles.Heading}
+        >
+          Let's Get Started
+        </Text>
       </View>
       <View style={MyStyles.InputContainer}>
         <Text>Name</Text>
