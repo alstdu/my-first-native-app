2c721c5a3156e6eb5b532d8ddd259f73cb1d1771
alstdu <lexi.dugo@gmail.com>
2024-06-25 20:40:34 -0700

    Create invite page

--- components/InviteScreen.js
+++ components/InviteScreen.js
@@ -1,10 +1,67 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { MyStyles } from "../MyStyles";
+import { View, Text, FlatList, Button, SafeAreaView, TouchableOpacity, } from "react-native";
+import MyStyles from "../MyStyles";
+
+const DATA = [
+  {
+    id: 'contact1',
+    photo: 'MyStyles.Circle',
+    title: 'Kreesha',
+    number: '123-456-7890',
+
+  },
+  {
+    id: 'contact2',
+    photo: 'string',
+    title: 'Gaby',
+    number: '123-456-7890',
+
+  },
+  {
+    id: 'contact3',
+    photo: 'string',
+    title: 'Mom',
+    number: '123-456-7890',
+  },
+  {
+    id: 'contact4',
+    photo: 'string',
+    title: 'Zack',
+    number: '123-456-7890',
+  },
+];
+
+// TODO: use photo
+//    For now we are using a circle as a placeholder, but eventually
+//    we will use an image from the users contacts
+const ContactView = ({ photo, title, number }) => (
+  <View style={{ flexDirection: 'row', gap: 10, marginTop: 10 }}>
+    <View style={MyStyles.Circle}></View>
+    <Text>{title}</Text>
+    <Text>{number}</Text>
+  </View>
+);
+
 export default function InviteScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Invite</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "flex-end", backgroundColor: "white" }}>
+      <View style={{ margin: 20, position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+        <Text style={MyStyles.Heading}>
+          Invite Your Friends
+        </Text>
+        <Text>Yoga is better together</Text>
+        {/* Just so I know the button is working since I don't have another page to navigate to yet */}
+        <TouchableOpacity onPress={() => alert('Hi!')}>
+          <Text style={MyStyles.TextButton}>Skip for now</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={MyStyles.InviteContainer}>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => <ContactView photo={item.photo} title={item.title} number={item.number} />}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </View>
   );
 }
