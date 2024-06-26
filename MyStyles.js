525810b9d326df49a7c0da5e37d2e8eae872e28d
alstdu <lexi.dugo@gmail.com>
2024-06-25 20:40:25 -0700

    update stylesheet

--- MyStyles.js
+++ MyStyles.js
@@ -1,49 +1,75 @@
 
 import { StyleSheet } from 'react-native';
-// GLOBAL STYLESHEET
 const MyStyles = StyleSheet.create({
-  ButtonContainer: {
-    padding: 10,
-    backgroundColor: '#366AF5',
-    borderRadius: 50,
-    width: 130,
-    marginTop: 10,
-  },
-  ButtonText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  Heading: {
-    fontWeight: 'bold',
-    fontSize: 36 ,
-    marginBottom: 10,
-    width: 150,
-  },
-  HomeBgImage: {
-    width: '100%',
-    height: '100%',
-    borderTopEndRadius: 190,
-    borderTopStartRadius: 190,
-    overflow: 'hidden',
-    marginTop: 70
-  },
-  input: {
-    width: "50%",
-    padding: 4,
-    backgroundColor: "#fff",
-    borderRadius: 30,
-    margin: 10
-  },
-  InputContainer: {
-    backgroundColor: '#DBDBD9',
-    width: '100%',
-    height: '75%',
-    borderTopStartRadius: 30,
-    borderTopEndRadius: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    TextButton: {
+        backgroundColor: 'none',
+        color: '#366AF5',
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    AddButton: {
+        padding: 8,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    ButtonContainer: {
+        padding: 10,
+        backgroundColor: '#366AF5',
+        borderRadius: 50,
+        width: 130,
+        marginTop: 10,
+    },
+    ButtonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    Heading: {
+        fontWeight: 'bold',
+        fontSize: 36,
+        marginBottom: 10,
+        width: 150,
+    },
+    HomeBgImage: {
+        width: '100%',
+        height: '100%',
+        borderTopEndRadius: 190,
+        borderTopStartRadius: 190,
+        overflow: 'hidden',
+        marginTop: 70
+    },
+    input: {
+        width: "50%",
+        padding: 4,
+        backgroundColor: "#fff",
+        borderRadius: 30,
+        margin: 10
+    },
+    InputContainer: {
+        backgroundColor: '#DBDBD9',
+        width: '100%',
+        height: '70%',
+        borderTopStartRadius: 30,
+        borderTopEndRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    InviteContainer: {
+        backgroundColor: '#DBDBD9',
+        width: '100%',
+        height: '60%',
+        borderTopStartRadius: 30,
+        borderTopEndRadius: 30,
+        alignItems: 'left',
+        justifyContent: 'center',
+        padding: 30,
+    },
+    Circle: {
+        backgroundColor: 'blue',
+        width: 30,
+        height: 30,
+        borderRadius: 50,
+    }
 });
 
 export default MyStyles;
